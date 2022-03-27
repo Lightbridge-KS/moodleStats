b90e4780aae239a1784ab85ce0e4ec4716eb46bf
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-03-27 22:13:16 +0700

    update test of summary_quiz()

--- tests/testthat/test-quiz.R
+++ tests/testthat/test-quiz.R
@@ -7,8 +7,8 @@ test_that("summary_quiz() works",{
 
   df <- summary_quiz(grades_df_preped)
   expect_equal(dim(df), c(1, 14))
-  expect_named(df, c("n", "min", "max", "max_quiz", "median",
-                     "Q1", "Q3", "IQR", "MAD", "mean", "SD",
+  expect_named(df, c("n", "min", "max", "max_quiz",
+                     "Q1","median", "Q3", "IQR", "MAD", "mean", "SD",
                      "skewness", "kurtosis", "Cronbach_Alpha"))
 
 })
